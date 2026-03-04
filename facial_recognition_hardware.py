1df857df8fa67297856ee6f4037fc273d577f43b
tsourosp <tsourosp@gmail.com>
2026-03-04 14:05:15 +0200

    Update facial_recognition_hardware.py

--- facial_recognition_hardware.py
+++ facial_recognition_hardware.py
@@ -1,4 +1,4 @@
-import face_recognition
+eimport face_recognition
 import cv2
 import numpy as np
 from picamera2 import Picamera2
@@ -32,24 +32,19 @@ start_time = time.time()
 fps = 0
 
 # List of names that will trigger the GPIO pin
-authorized_names = ["john", "alice", "bob"]  # Replace with names you wish to authorise THIS IS CASE-SENSITIVE
+authorized_names = ["metse","kvtsakos"] 
 
 def process_frame(frame):
     global face_locations, face_encodings, face_names
     
-    # Resize the frame using cv_scaler to increase performance (less pixels processed, less time spent)
     resized_frame = cv2.resize(frame, (0, 0), fx=(1/cv_scaler), fy=(1/cv_scaler))
     
-    # Convert the image from BGR to RGB colour space, the facial recognition library uses RGB, OpenCV uses BGR
     rgb_resized_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
-    
-    # Find all the faces and face encodings in the current frame of video
     face_locations = face_recognition.face_locations(rgb_resized_frame)
     face_encodings = face_recognition.face_encodings(rgb_resized_frame, face_locations, model='large')
     
     face_names = []
     authorized_face_detected = False
-    
     for face_encoding in face_encodings:
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
@@ -65,7 +60,7 @@ def process_frame(frame):
                 authorized_face_detected = True
         face_names.append(name)
     
-    # Control the GPIO pin based on face detection
+   
     if authorized_face_detected:
         output.on()  # Turn on Pin
     else:
